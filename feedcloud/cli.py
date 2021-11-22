5afb7e243b81a3d1785729f9793cccca9e16bd98
Mohammad Banisaeid <smbl64@gmail.com>
2021-11-22 13:17:48 +0100

    Add two CLI commands for managing users

--- feedcloud/cli.py
+++ feedcloud/cli.py
@@ -27,26 +27,58 @@ def init_database(delete_all):
     click.echo("Done")
 
 
-@cli.command()
-def create_admin_user():
-    """
-    Create the default admin user.
-    """
-    admin_password = constants.DEFAULT_ADMIN_USER
+@cli.group("user")
+def user_group():
+    pass
 
+
+@user_group.command()
+def create_root():
+    """
+    Create the default root user.
+    """
+    create_user(
+        constants.DEFAULT_ADMIN_USER, constants.DEFAULT_ADMIN_USER, is_admin=True
+    )
+
+
+@user_group.command("create")
+@click.option("--username", "-u", required=True)
+@click.password_option()
+def create_normal_user(username: str, password: str) -> None:
+    """
+    Create a new user.
+    """
+    print(password)
+    create_user(username, password, is_admin=False)
+
+
+def create_user(username: str, password: str, *, is_admin: bool) -> None:
     User = database.User
 
     with database.get_session() as session:
-        if session.query(User.username == constants.DEFAULT_ADMIN_USER).count() != 0:
-            click.echo(f"User '{constants.DEFAULT_ADMIN_USER}' already exists.")
+        if session.query(User.username == username).count() != 0:
+            click.echo(f"User '{username}' already exists.")
             return
 
         user = User(
-            username=constants.DEFAULT_ADMIN_USER,
-            password_hash=helpers.hash_password(admin_password),
-            is_admin=True,
+            username=username,
+            password_hash=helpers.hash_password(password),
+            is_admin=is_admin,
         )
         session.add(user)
         session.commit()
 
+# @cli.command()
+# def woot():
+#     with database.get_session() as session:
+#         user = database.User(username="foo", password_hash="bar")
+#         feed = database.Feed(url="https://www.nu.nl/rss/Algemeen", user=user)
+#         session.add(feed)
+#         session.commit()
+#         session.refresh(feed)
 
+#     print(feed.url)
+
+#     worker = FeedWorker(feed)
+#     worker.start()
